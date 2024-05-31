c19185e0bdc7a54f87bd5aed21b7d30cbc123969
ThinkLee <dinhlethinh137@gmail.com>
2024-06-01 01:09:35 +0700

    AccountDAO IdGenerator added

--- src/java/dao/AccountDAO.java
+++ src/java/dao/AccountDAO.java
@@ -5,10 +5,55 @@
  */
 package dao;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 /**
  *
  * @author dinhl
  */
 public class AccountDAO {
     
+    //Auto generate CustomerId by Time
+    public static String generateId(){
+        //Get time
+        LocalDateTime now = LocalDateTime.now();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyMMddHHmmssSSS");
+        
+        //Format getted time
+        String formattedDateTime = now.format(formatter);
+        String alphabet = decimalToAlphabet(Long.parseLong(formattedDateTime));
+        String id = "CUS" + alphabet;
+
+        return id;
+    }
+    
+    //Convert decimal to alphabet by ASCII
+    public static String decimalToAlphabet(long number) {
+        if (number <= 0) {
+            throw new IllegalArgumentException("Number must be greater than 0");
+        }
+
+        StringBuilder result = new StringBuilder();
+
+        while (number > 0) {
+            number--;
+            char digit;
+            if (number % 36 < 10) {
+                digit = (char) ('0' + (number % 36)); // Map to '0'-'9'
+            } else {
+                digit = (char) ('a' + (number % 36 - 10)); // Map to 'a'-'z'
+            }
+            result.insert(0, digit);
+            number = number / 36;
+        }
+
+        return result.toString().toUpperCase();
+    }
+    
+    public static void main(String[] args) {
+        String id = generateId();
+        System.out.println(id);
+    }
+
 }
